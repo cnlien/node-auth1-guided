23fe61a98c59b4a46cb0ff627ff2fe147083c538
Chris Lien <clien@sharpencx.com>
2020-09-30 11:28:07 -0400

    Hooked up bcrypt to the register function to hash the password

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -3,18 +3,14 @@ const users = require('../users/users-model.js');
 const bcrypt = require('bcryptjs');
 
 router.post('/register', async (req, res, next) => {
-
-    // DON'T FORGET TO SEED THE DB AFTER THIS
-    // knex migrate:latest
-    // knex see:run
-
     let user = req.body;
+    const hash = brcypt.hashSync(user.password, 10);
+    user.password = hash
 
     try {
         const saved = await users.add(user);
         res.status(201).json(saved);
     }
-    
     catch (err) {
         next({
             apiCode: 500,
