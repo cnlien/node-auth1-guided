4f7cd462f9816928dc52c70f17fb80281b85b340
Chris Lien <clien@sharpencx.com>
2020-09-30 10:55:53 -0400

    refactored error handle to use ternary operators

--- api/errorHandler.js
+++ api/errorHandler.js
@@ -1,12 +1,21 @@
 module.exports = (err, req, res, next) => {
     console.log('express error: ', err);
-    if (err.apiCode >= 400) {
-        res.status(err.apiCode).json({
+
+    err.apiCode >= 400
+        ? res.status(err.apiCode).json({
             apiCode: err.apiCode,
             apiMessage: err.apiMessage,
             ...err
         })
-    } else {
-        next();
-    }
+        : next();
 }
+
+    // if (err.apiCode >= 400) {
+    //     res.status(err.apiCode).json({
+    //         apiCode: err.apiCode,
+    //         apiMessage: err.apiMessage,
+    //         ...err
+    //     })
+    // } else {
+    //     next();
+    // }
