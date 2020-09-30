9a6692a1e09af122a10cba6bb543aa333221a535
Chris Lien <clien@sharpencx.com>
2020-09-30 10:47:08 -0400

    Added register api post request

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,5 +1,18 @@
 const router = require('express').Router();
 const users = require('../users/users-model.js');
 
+router.post('/register', async (req, res, next) => {
+    let user = req.body;
+
+    try {
+        const saved = await users.add(user);
+        res.status(201).json(saved);
+    }
+    
+    catch (err) {
+        
+    }
+})
+
 
 module.exports = router
