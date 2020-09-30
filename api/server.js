8787acbc7b71800f3bf76a323616364ca18d9a5c
Chris Lien <clien@sharpencx.com>
2020-09-30 12:14:46 -0400

    configured express-session on server.js

--- api/server.js
+++ api/server.js
@@ -2,11 +2,26 @@ const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
 const errorHandler = require("./errorHandler.js")
+const session = require('express-session');
 
 const usersRouter = require("../users/users-router.js");
 const authRouter = require('../auth/auth-router.js')
 
 const server = express();
+
+const sessionConfig = {
+  name: "sessionCookie",
+  secret: "not typically stored here",
+  cookie: {
+    maxAge: 60 * 60 * 1000,
+    secure: false, // TYPICALLY THIS IS `TRUE` BUT FOR THIS PROJECT WE DON'T MIND IF THE REQUEST IS NOT SENT OVER HTTPS
+    httpOnly: true,
+  },
+  resave: false,
+  saveUninitialized: false
+}
+
+server.use(session(sessionConfig));
 
 server.use(helmet());
 server.use(express.json());
